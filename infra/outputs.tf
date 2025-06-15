271eaad8e6cc70b310579a365ae8e9e6a27baf5e
Neelam Gauchan <gauchanneelam@gmail.com>
2025-06-15 14:03:29 -0400

    Testing

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "frontend_url" {
-  description = "Public URL of the frontend container app"
-  value       = azurerm_container_app.frontend.ingress[0].fqdn
-}
-
-output "backend_internal_url" {
-  description = "Internal FQDN of the backend container app"
-  value       = azurerm_container_app.backend.ingress[0].fqdn
-}
--- infra/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-# Use custom values if needed
-location            = "eastus"
-resource_group_name = "TradeMinutes-RG"
-acr_name            = "trademinutesacr"
-env_name            = "trademinutes-env"
-frontend_name       = "trademinutes-frontend"
-backend_name        = "trademinutes-users-api"
